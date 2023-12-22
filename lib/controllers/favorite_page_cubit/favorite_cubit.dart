a7a22689aff81d17d81fa8530e0dd2e562441e38
MohammadNasser-CS <abualnasser198@gmail.com>
2023-12-22 21:20:45 +0200

    favorite problem simple solutions

--- lib/controllers/favorite_page_cubit/favorite_cubit.dart
+++ lib/controllers/favorite_page_cubit/favorite_cubit.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
-import 'package:e_commerce/controllers/home_tab_view_cubit/home_cubit.dart';
 import 'package:e_commerce/models/product_item_modle.dart';
 import 'package:meta/meta.dart';
 
